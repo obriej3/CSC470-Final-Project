cf9f0aa8822443dce6ed1442163c036c61914454
nicolekercado <kercadn1@tcnj.edu>
2023-04-08 22:49:01 -0400

    Camera rotation implemented

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -43,50 +43,6 @@ public class playerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
-        /* // If S key is down, walk backwards
-        if(Input.GetKey(KeyCode.S))
-        {
-            anim.SetBool("isWalkingBackwards", true);
-        }
-        else
-        {
-            anim.SetBool("isWalkingBackwards", false);
-        }
-
-        // If D key is down, strafe right
-        if(Input.GetKey(KeyCode.D))
-        {
-            anim.SetBool("isStrafingRight", true);
-        }
-        else
-        {
-            anim.SetBool("isStrafingRight", false);
-        }
-
-        // If A key is down, strafe left
-        if(Input.GetKey(KeyCode.A))
-        {
-            anim.SetBool("isStrafingLeft", true);
-        }
-        else
-        {
-            anim.SetBool("isStrafingLeft", false);
-        }
-
-        if(anim.GetBool("isRunning") == false & anim.GetBool("isJumping") == false & anim.GetBool("isWalkingBackwards") == false & anim.GetBool("isStrafingRight") == false & anim.GetBool("isStrafingLeft") == false)
-        {
-            anim.SetBool("isIdle", true);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Mouse0) & anim.GetBool("isIdle") == true)
-        {
-            anim.SetBool("isAttacking", true);
-        }
-        else
-        {
-            anim.SetBool("isAttacking", false);
-        }
-        */
     } 
 
     private void Step()
