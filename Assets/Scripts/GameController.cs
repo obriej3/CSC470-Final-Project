27f76957cc1c231b745fd4ef4cd0a4b5ea1fe47d
nicolekercado <kercadn1@tcnj.edu>
2023-04-10 12:37:50 -0400

    Added some UI menu functionality

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    // Global variables
+    public GameObject menuPanel;
+    public GameObject instructionsPanel;
+    public GameObject menuButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuButton.SetActive(true);
+        menuPanel.SetActive(false);
+        instructionsPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadMenu()
+    {
+        menuPanel.SetActive(true);
+        menuButton.SetActive(false);
+    }
+
+    public void ExitMenu()
+    {
+        menuPanel.SetActive(false);
+        menuButton.SetActive(true);
+    }
+
+    public void LoadInstructions()
+    {
+        instructionsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void ExitInstructions()
+    {
+        instructionsPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
 }
