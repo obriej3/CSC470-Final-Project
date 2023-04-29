a46cacfdfe77a9941214a3bcb2745d49c21773c1
nicolekercado <kercadn1@tcnj.edu>
2023-04-29 12:20:44 -0400

    Implemented exit game button/functionality and made main game terrain smaller

--- Assets/StartMenuController.cs
+++ Assets/StartMenuController.cs
@@ -21,4 +21,10 @@ public class StartMenuController : MonoBehaviour
     {
         SceneManager.LoadScene("MainGameFarm");
     }
+
+    public void ExitGame()
+    {
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
 }
